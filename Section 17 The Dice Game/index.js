7c2dc5fc892dbca9bdd45dec8edc65f068b466a8
Rommer Chu <rommer.chu@gmail.com>
2026-04-03 20:32:34 -0400

    dice challenge
    dice challenge

---
+++ Section 17 The Dice Game/index.js
@@ -0,0 +1,25 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+//document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+document.getElementsByTagName("img")[0].setAttribute("src", randomImageSource1);
+
+//var randomNumber1 = Math.floor(Math.random() * 6) + 1; //get randon number
+//var randomDiceImage = "dice" + randomNumber1; //dice1.png
+//var randomImageSource = "images/" + randomDiceImage + ".png"; //image/dice1.png
+//var image1 = document.querySelectorAll("img")[0];
+//image1.setAttribute("src", randomImageSource);
+
+console.log(randomNumber1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.getElementsByTagName("img")[1].setAttribute("src", randomImageSource2);
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
