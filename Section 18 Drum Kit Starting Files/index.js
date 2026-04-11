e8bcc62167afd0f06313b294c1324d9dd4806aa3
Rommer Chu <rommer.chu@gmail.com>
2026-04-10 20:25:33 -0400

    drum kits
    drum kit

--- Section 18 Drum Kit Starting Files/index.js
+++ Section 18 Drum Kit Starting Files/index.js
@@ -2,12 +2,53 @@ var numOfButton = document.querySelectorAll(".drum").length;
 
 for (let x = 0; x < numOfButton; x++) {
   document.querySelectorAll(".drum")[x].addEventListener("click", function () {
-    console.log(this);
+    var buttonInnerHtml = this.innerHTML;
 
+    switch (buttonInnerHtml) {
+      case "w":
+        var sound = new Audio("sounds/tom-1.mp3");
+        sound.play();
+        break;
+      case "a":
+        var sound = new Audio("sounds/tom-2.mp3");
+        sound.play();
+        break;
+      case "s":
+        var sound = new Audio("sounds/tom-3.mp3");
+        sound.play();
+        break;
+      case "d":
+        var sound = new Audio("sounds/tom-4.mp3");
+        sound.play();
+        break;
+      case "j":
+        var sound = new Audio("sounds/crash.mp3");
+        sound.play();
+        break;
+      case "k":
+        var sound = new Audio("sounds/snare.mp3");
+        sound.play();
+        break;
+      case "l":
+        var sound = new Audio("sounds/kick-bass.mp3");
+        sound.play();
+        break;
+
+      default:
+        console.log(this.buttonInnerHtml);
+        break;
+    }
+
+    //if(buttonInnerHtml == "w"){
+    // var sound = new Audio("sounds/tom-1.mp3");
+    //       sound.play();
+    //}
+
+    //console.log(this.);
     //alert("Test");
   });
 }
-//var sound = new Audio("sounds/crash.mp3");
-// if (sound == numOfButton[1]) {
-//   sound.play();
-// }
+// var sound = new Audio("sounds/crash.mp3");
+// sound.play();
+
+//this.style.color = "white";
