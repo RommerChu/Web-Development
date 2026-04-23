1b54002853c9a36632270f402002a271122187e0
Rommer Chu <rommer.chu@gmail.com>
2026-04-22 22:08:17 -0400

    JQUERY
    TOGGLE, HIDE, REMOVE, CSS, SLIDEUP, SLIDEDOWN

--- Section 19 JQUERY/index.js
+++ Section 19 JQUERY/index.js
@@ -1,1 +1,57 @@
-document.querySelector("h1").innerHTML = "Romer";
+//document.querySelector("h1").innerHTML = "Hello Rommer!";
+//$("h1").html("Mr. Rommer");
+
+//Buttons
+// for (let i = 0; i < 5; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", function () {
+//     document.querySelector("h1").style.color = "red";
+//   });
+// }
+
+//CSS color red
+// $("button").click(() => {
+//   $("h1").css("color", "red");
+// });
+
+//REMOVE BUTTONS
+// $("button").click(() => {
+//   $("h1").remove();
+// });
+
+//HIDE BUTTONS
+// $("button").click(() => {
+//   $("h1").hide();
+// });
+
+//TOGGLE BUTTONS
+// $("button").click(() => {
+//   $("h1").toggle();
+// });
+
+//FADE-TOGGLE BUTTONS
+$("button").click(() => {
+  $("h1").fadeToggle();
+});
+
+//Input
+// $("input").keypress(function (e) {
+//   console.log(e.key);
+// });
+
+//Return result to H1 .text
+$("input").keypress(function (e) {
+  //console.log(e.key);
+  $("h1").text(e.key);
+});
+
+//ADD new Button BEFORE
+$("h1").before("<button>New Before</button>");
+
+//ADD new Button PREPEND
+$("h1").prepend("<button>New Prepend</button>");
+
+//ADD new Button APPEND
+$("h1").append("<button>New Append</button>");
+
+//ADD new Button AFTER
+$("h1").append("<button>New After</button>");
