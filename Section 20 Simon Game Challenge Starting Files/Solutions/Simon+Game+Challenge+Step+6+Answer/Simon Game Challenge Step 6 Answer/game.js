50bcd7b2f08ab588b0c043084c3adf1338b7c864
Rommer Chu <rommer.chu@gmail.com>
2026-05-13 00:01:29 -0400

    Simon Game
    Simon Game

--- Section 20 Simon Game Challenge Starting Files/Solutions/Simon+Game+Challenge+Step+6+Answer/Simon Game Challenge Step 6 Answer/game.js
+++ Section 20 Simon Game Challenge Starting Files/Solutions/Simon+Game+Challenge+Step+6+Answer/Simon Game Challenge Step 6 Answer/game.js
@@ -1,9 +1,11 @@
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 var userClickedPattern = [];
 
-$(".btn").click(function () {
+$(".btn").click(function() {
+
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
@@ -13,14 +15,12 @@ $(".btn").click(function () {
 });
 
 function nextSequence() {
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  $("#" + randomChosenColour)
-    .fadeIn(100)
-    .fadeOut(100)
-    .fadeIn(100);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
   playSound(randomChosenColour);
 }
@@ -32,6 +32,7 @@ function playSound(name) {
 
 //1. Create a new function called animatePress(), it should take a single input parameter called currentColour.
 function animatePress(currentColor) {
+
   //2. Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
   $("#" + currentColor).addClass("pressed");
 
