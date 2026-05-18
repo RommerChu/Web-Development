6fcc7627b93dfb005d5741455abafa6dc499005b
Rommer Chu <rommer.chu@gmail.com>
2026-05-18 19:08:02 -0400

    node js and npm
    node js and npm

---
+++ Section 23 Node JS/2.1 Using Node/index.js
@@ -0,0 +1,4 @@
+console.log("Hello World");
+var name = "Rommer";
+var last = "Chu";
+console.log(name + last);
---
+++ Section 23 Node JS/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+//var generateName = require("sillyname");
+import superheroes from "superheroes";
+const name = superheroes.random();
+console.log(`I am  ${name}`);
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}`);
