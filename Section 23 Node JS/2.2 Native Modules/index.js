bf5e8fdaccc622c93b67c3edbe2f4f40a4874443
Rommer Chu <rommer.chu@gmail.com>
2026-06-14 21:16:30 -0400

    QR Code
    QR Code

--- Section 23 Node JS/2.2 Native Modules/index.js
+++ Section 23 Node JS/2.2 Native Modules/index.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 
 //------TO WRITE-CREATED A FILE------
-// fs.writeFile("message.txt", "Heelo from NJ", (err) => {
+// fs.writeFile("message.txt", "Hello Mr. Chu for NJ", (err) => {
 //   if (err) throw err;
 //   console.log("The file has been saved!");
 // });
 
-//TO READ FILE
+//------TO READ FILE------
 fs.readFile("./message.txt", "utf8", (err, data) => {
   if (err) throw err;
   console.log(data);
