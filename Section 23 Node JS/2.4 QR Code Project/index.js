556473b706234b27423f5624993459f3a58895c3
Rommer Chu <rommer.chu@gmail.com>
2026-06-16 22:55:43 -0400

    qr code
    qr code

--- Section 23 Node JS/2.4 QR Code Project/index.js
+++ Section 23 Node JS/2.4 QR Code Project/index.js
@@ -1,13 +1,24 @@
 /* 1. Use the inquirer npm package to get user input.*/
 import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+//var qr = require("qr-image");
 
 inquirer
-    .prompt([{
-        /* Pass your questions in here */
-        massage: "please ty your url", name: "URL"
-    }]);
+  .prompt([
+    {
+      /* Pass your questions in here */
+      message: "Please type your url:",
+      name: "URL",
+    },
+  ])
   .then((answers) => {
     // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+    //console.log(answers);
   })
   .catch((error) => {
     if (error.isTtyError) {
